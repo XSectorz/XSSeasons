888e903de5b13b792c128d3ad1241b488667efdd
XSectorz <81065047+XSectorz@users.noreply.github.com>
2023-07-27 16:59:42 +0700

    Delete XSSeasons directory

--- XSSeasons/src/main/java/net/xsapi/panat/xsseasons/configurations/loadConfig.java
+++
@@ -1,9 +0,0 @@
-package net.xsapi.panat.xsseasons.configurations;
-
-public class loadConfig {
-
-    public loadConfig() {
-        new config().loadConfigu();
-        new data().loadConfigu();
-    }
-}
--- XSSeasons/src/main/java/net/xsapi/panat/xsseasons/core/Seasons.java
+++
@@ -1,5 +0,0 @@
-package net.xsapi.panat.xsseasons.core;
-
-public enum Seasons {
-    Summer,Fall,Spring,Winter
-}
